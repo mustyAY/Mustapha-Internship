104732bdd1d099eee8933de32439c040484b6532
mustyAY <mustyyahya.1580@yahoo.com>
2023-11-06 23:30:14 +0100

    Explore Page complete

--- src/components/UI/CountdownTimer.jsx
+++ src/components/UI/CountdownTimer.jsx
@@ -5,12 +5,8 @@ function CountdownTimer({ expiryDate }) {
     const calculateRemainingTime = (expiryDate) => {
         const now = Date.now();
         const countdown = expiryDate - now;
-        if (countdown < 0) {
-              return "EXPIRED"
-            } else {
-              return countdown;
-            }
-    }
+        return Math.max(0, countdown);
+    };
 
     const [remainingTime, setRemainingTime] = useState(calculateRemainingTime(expiryDate));
 
@@ -19,7 +15,7 @@ function CountdownTimer({ expiryDate }) {
             const newRemainingTime = calculateRemainingTime(expiryDate);
             setRemainingTime(newRemainingTime);
 
-            if (newRemainingTime <=0) {
+            if (newRemainingTime <= 0) {
                 clearInterval(intervalId);
             }
         }, 1000);
@@ -29,19 +25,29 @@ function CountdownTimer({ expiryDate }) {
         };
     }, [expiryDate]);
 
-    function formatTime(remainingTime){
+    function formatTime(remainingTime) {
         const secondsLeft = remainingTime / 1000;
         const minutesLeft = secondsLeft / 60;
         const hoursLeft = minutesLeft / 24;
-    
+
         const secondsText = Math.floor(secondsLeft % 60);
         const minutesText = Math.floor(minutesLeft % 60);
         const hoursText = Math.floor(hoursLeft);
-    
-        return `${hoursText}h ${minutesText}m ${secondsText}s`;
-      }
 
-    return <div className="de_countdown">{formatTime(remainingTime)}</div>
+        return `${hoursText}h ${minutesText}m ${secondsText}s`;
+    }
+
+    return (
+        <>
+            {
+                remainingTime ?
+                    <div className="de_countdown">{formatTime(remainingTime)}</div>
+                    :
+                    <div className="de_countdown">EXPIRED</div>
+            }
+        </>
+
+    )
 }
 
 export default CountdownTimer;
